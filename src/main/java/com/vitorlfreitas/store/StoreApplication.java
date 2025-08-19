4171a1eab1ed2d8f901945aca0e5d0f6d99677c7
vitorlfreitas <vitor.lucfreitas@gmail.com>
2025-08-19 23:50:36 +0100

    Adds a new component and implements the lazy feature of spring that allows laze initialization. @See Lesson 11 of Spring boot course

--- src/main/java/com/vitorlfreitas/store/StoreApplication.java
+++ src/main/java/com/vitorlfreitas/store/StoreApplication.java
@@ -10,9 +10,14 @@ public class StoreApplication {
 	public static void main(String[] args) {
 
 		ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-		var orderService = context.getBean(OrderService.class);
 
-		orderService.placeOrder();
+
+		// Run the application with the code below commented, then uncomment the code and try again
+
+//		var orderService = context.getBean(HeavyResource.class);
+
+		// You will notice the component is created only when needed
+
 	}
 
 }
