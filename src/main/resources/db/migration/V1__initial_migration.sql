40f3ac15a8043e56d91ff6ccbf2b3b2f1042fbaf
vitorlfreitas <vitor.lucfreitas@gmail.com>
2025-09-09 19:13:56 +0100

    Using Flyway, we add a sql script which defines two entities, addresses and users, using the convention required by flyway. Then, we add a new version that add a new column to the table address. After that, we run the code and we now have 2 tables in our store database. This class is related to the introduction to flyway and how to use and manage versions.

---
+++ src/main/resources/db/migration/V1__initial_migration.sql
@@ -0,0 +1,18 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT users_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE addresses (
+    id INT AUTO_INCREMENT,
+    street VARCHAR(50) NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    eircode VARCHAR(7) NOT NULL UNIQUE,
+    user_id INT NOT NULL,
+    CONSTRAINT address_pk PRIMARY KEY (id),
+    CONSTRAINT addresses_user_id_fk FOREIGN KEY (user_id) REFERENCES users(id)
+);
